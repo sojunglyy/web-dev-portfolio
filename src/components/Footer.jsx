e286020c335f45b052c0eddf6ba5ededa5213e3d
Sojung <sojunglyy@gmail.com>
2026-07-11 22:28:59 +0200

    update links in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,19 @@ const Footer = () => {
   return (
     <section className="w-full h-[5vh] fixed bottom-0 bg-black border-t">
       <div className="w-full flex flex-row gap-5">
-        <p>Email</p>
-        <a href="">
+        <p>
+          <a href="mailto:sojunglyy@gmail.com">Email</a>
+        </p>
+        <a href="https://github.com/sojunglyy" target="_blank">
           <p>Github</p>
         </a>
-        <a href="">
+        <a href="https://www.instagram.com/sopppppppo/" target="_blank">
           <p>Instagram</p>
         </a>
-        <a href="">
+        <a
+          href="https://www.linkedin.com/in/sojung-lee-61a0b2246/"
+          target="_blank"
+        >
           <p>LinkedIn</p>
         </a>
       </div>
